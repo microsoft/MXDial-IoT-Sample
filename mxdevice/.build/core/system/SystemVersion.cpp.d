c560bacf0d810a23a0198512bf477a573280b7c5
James Carpinter <james@zarmada.com>
2018-09-04 14:56:48 +1200

    Remove build artifacts; rename file

--- mxdevice/.build/core/system/SystemVersion.cpp.d
+++
@@ -1,4 +0,0 @@
-c:\LocalCode\fitbit-demos\mx-device\mx-app\.build\core\system\SystemVersion.cpp.o: \
- C:\Users\jamiewilbraham\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.3.2\cores\arduino\system\SystemVersion.cpp \
- C:\Users\jamiewilbraham\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.3.2\cores\arduino/system/mbed_config.h \
- C:\Users\jamiewilbraham\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.3.2\cores\arduino\system\SystemVersion.h
