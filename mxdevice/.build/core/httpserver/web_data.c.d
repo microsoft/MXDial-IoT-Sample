cace17bf209f05d3228ac81b93fc2a5985340061
jamie wilbraham <jamie@zarmada.com>
2018-09-05 14:05:11 +1200

    rem build

--- mxdevice/.build/core/httpserver/web_data.c.d
+++
@@ -1,3 +0,0 @@
-c:\LocalCode\fitbit-demos\mx-device\mx-app\.build\core\httpserver\web_data.c.o: \
- C:\Users\jamiewilbraham\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.3.2\cores\arduino\httpserver\web_data.c \
- C:\Users\jamiewilbraham\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.3.2\cores\arduino/system/mbed_config.h
